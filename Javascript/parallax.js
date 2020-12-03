8aa057888dacc200141f1e1268bcd5d1c5270e81
Suna24 <bastien.t2002@gmail.com>
2020-12-03 17:23:25 +0100

    V10

--- Javascript/parallax.js
+++
@@ -1,5 +0,0 @@
-/* Fonction qui permet de faire l'effet parallax */
-$(document).ready(function(){		
-    $('#slide1').parallax("center", 0, 0.1, true);
-    $('#slide2').parallax("center", 900, 0.1, true);
-})
